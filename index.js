a09cbf5b75812617e1fdbfd314f90009ff4e44f8
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-15 16:19:06 +0530

    docs:if else statment

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const billAmount = document.querySelector('#bill-amount');
-const cashGiven= document.querySelector('#cash-given');
+const cashGiven = document.querySelector('#cash-given');
 const checkCash = document.querySelector('#check');
 const noOfNotes = document.querySelectorAll('.noOfNotes');
+const errorMessage = document.querySelector('#message');
 
+checkCash.addEventListener('check', function validateBillAmountAndCash(){
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+
+        } 
+        else {
+            showMessage("Do You Want to Wash Plates");
+        }
+    } else {
+        errorMessage.innerText = ("Enter Valid Amount..!")
+    }
+
+});
