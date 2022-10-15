df18f30527f3743781e9387b09bb349041a5ddeb
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-15 16:35:19 +0530

    docs: bill amount and cash given update

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const billAmount = document.querySelector('#bill-amount');
 const cashGiven = document.querySelector('#cash-given');
-const checkCash = document.querySelector('#check');
+const checkCash = document.querySelector('#checkbtn');
 const noOfNotes = document.querySelectorAll('.noOfNotes');
 const errorMessage = document.querySelector('#message');
 
-checkCash.addEventListener('check', function validateBillAmountAndCash(){
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-
-        } 
-        else {
-            showMessage("Do You Want to Wash Plates");
-        }
+checkCash.addEventListener("click", function validateBillAmountAndCash(){
+    errorMessage.style.display = "none";
+     if (billAmount.value > 0) {
+       if (cashGiven.value >= billAmount.value) {
+        
+       }
+       else {
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Do You Want to do the dishes";
+       }
     } else {
-        errorMessage.innerText = ("Enter Valid Amount..!")
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Enter Valid Amount..!";
     }
 
 });
