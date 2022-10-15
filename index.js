31da632329c6faf8a82cd299c215fd08419cae92
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-15 15:54:17 +0530

    docs: select id and class

---
+++ index.js
@@ -0,0 +1,5 @@
+const billAmount = document.querySelector('#bill-amount');
+const cashGiven= document.querySelector('#cash-given');
+const checkCash = document.querySelector('#check');
+const noOfNotes = document.querySelectorAll('.noOfNotes');
+
